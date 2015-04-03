openstack/puppet-cinder

Makes kombu_ssl_* parameters optional when rabbit_use_ssl => true
The kombu_ssl_* parameters should not be required when rabbit_use_ssl => true
Rather, rabbit_use_ssl must be set to true if the kombu_ssl_* parameters are
used.

Change-Id: I5e138adea60d2dc8b398bb05896def8508cd9f44
Closes-Bug: 1356083

spec/classes/cinder_spec.rb
@@ -203,7 +203,7 @@ describe 'cinder' do
     it { should contain_cinder_config('DEFAULT/qpid_sasl_mechanisms').with_value('DIGEST-MD5 GSSAPI PLAIN') }
   end
 
-  describe 'with SSL enabled' do
+  describe 'with SSL enabled with kombu' do
     let :params do
       req_params.merge!({
         :rabbit_use_ssl     => true,
@@ -219,6 +219,22 @@ describe 'cinder' do
       should contain_cinder_config('DEFAULT/kombu_ssl_ca_certs').with_value('/path/to/ssl/ca/certs')
       should contain_cinder_config('DEFAULT/kombu_ssl_certfile').with_value('/path/to/ssl/cert/file')
       should contain_cinder_config('DEFAULT/kombu_ssl_keyfile').with_value('/path/to/ssl/keyfile')
+      should contain_cinder_config('DEFAULT/kombu_ssl_version').with_value('TLSv1')
+    end
+  end
+
+  describe 'with SSL enabled without kombu' do
+    let :params do
+      req_params.merge!({
+        :rabbit_use_ssl     => true,
+      })
+    end
+
+    it do
+      should contain_cinder_config('DEFAULT/rabbit_use_ssl').with_value('true')
+      should contain_cinder_config('DEFAULT/kombu_ssl_ca_certs').with_ensure('absent')
+      should contain_cinder_config('DEFAULT/kombu_ssl_certfile').with_ensure('absent')
+      should contain_cinder_config('DEFAULT/kombu_ssl_keyfile').with_ensure('absent')
       should contain_cinder_config('DEFAULT/kombu_ssl_version').with_value('TLSv1')
     end
   end

msolberg/openstack-puppet-modules

(cherry picked from commit 1c3fd4da9e329400e7c92196a8ddd6dd8b95dafe)

cinder/spec/classes/cinder_spec.rb
@@ -178,7 +178,7 @@ describe 'cinder' do
     it { should contain_cinder_config('DEFAULT/qpid_sasl_mechanisms').with_value('DIGEST-MD5 GSSAPI PLAIN') }
   end
 
-  describe 'with SSL enabled' do
+  describe 'with SSL enabled with kombu' do
     let :params do
       req_params.merge!({
         :rabbit_use_ssl     => true,
@@ -194,6 +194,22 @@ describe 'cinder' do
       should contain_cinder_config('DEFAULT/kombu_ssl_ca_certs').with_value('/path/to/ssl/ca/certs')
       should contain_cinder_config('DEFAULT/kombu_ssl_certfile').with_value('/path/to/ssl/cert/file')
       should contain_cinder_config('DEFAULT/kombu_ssl_keyfile').with_value('/path/to/ssl/keyfile')
+      should contain_cinder_config('DEFAULT/kombu_ssl_version').with_value('SSLv3')
+    end
+  end
+
+  describe 'with SSL enabled without kombu' do
+    let :params do
+      req_params.merge!({
+        :rabbit_use_ssl     => true,
+      })
+    end
+
+    it do
+      should contain_cinder_config('DEFAULT/rabbit_use_ssl').with_value('true')
+      should contain_cinder_config('DEFAULT/kombu_ssl_ca_certs').with_ensure('absent')
+      should contain_cinder_config('DEFAULT/kombu_ssl_certfile').with_ensure('absent')
+      should contain_cinder_config('DEFAULT/kombu_ssl_keyfile').with_ensure('absent')
       should contain_cinder_config('DEFAULT/kombu_ssl_version').with_value('SSLv3')
     end
   end